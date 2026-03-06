f1a43dc79961adc4836106f3285a512b28571e28
Aiman <aimanfatima384@gmail.com>
2026-03-06 10:14:12 -0600

    -

---
+++ arrays/sketch.js
@@ -0,0 +1,15 @@
+// Arrays
+// Aiman Fatima Adnan
+// 05/03/26
+
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
--- index.md
+++ index.md
@@ -2,7 +2,8 @@
 
 ## Demos
 - [First Day Demo](01 - first demo)
-- [generative art] (01-artP)
+- [generative art] (01-art)
+- [arrays assignment] (arrays)
 
 ## Assignments
 - assignments will be put here
