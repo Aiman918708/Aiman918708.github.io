f15ea22b7f7a08b3f952fe430c8d315cbc076a4a
Aiman <aimanfatima384@gmail.com>
2026-03-16 10:55:38 -0600

    started project today, found reference picture for art

--- arrays/sketch.js
+++ arrays/sketch.js
@@ -1,21 +1,19 @@
-// Arrays
+// Arrays - Generative Art
 // Aiman Fatima Adnan
 // 05/03/26
 
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - 
 
 // Variables
 
-let glass = [];
-let ball = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('aqua');
+  
 }
 
 function draw() {
-  text('Click To Sort The Colors');
+  
 }
 
