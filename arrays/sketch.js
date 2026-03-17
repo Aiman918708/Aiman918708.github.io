e33e3b3fc93a55ef64dec00e99e822c6746ac0aa
Aiman918708 <aimanfatima384@gmail.com>
2026-03-17 15:20:46 -0600

    Update sketch.js

--- arrays/sketch.js
+++ arrays/sketch.js
@@ -3,30 +3,36 @@
 // 16/03/26
 
 // Extra for Experts:
-// - 
+// Using HSB instead of RGB values to control the brightness, saturation and transparency
+// of colors
 
 // Creating Arrays
 let picture = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('black');
+  colorMode(HSB, 300, 100, 100, 100);
+  background(0); // 0 in HSB = to black
   
   // Creates circles of random width, height and size, and adds them to the array
   for (let i = 0; i < 20; i++) {
     let circles = {
       x: random(windowWidth),
       y: random(windowHeight),
-      radius: random(20,200)
+      radius: random(20,200),
+      c: color(random(300, 360), 100, 100, 20)
     };
     picture.push(circles);
   }
 }
 
 function draw() {
-  background('black');
-  for (let i = 0; i < pictures.length; i++) {
-    if (i = circles)
+  // Draws the circles in the list onto the screen
+  for (let i = 0; i < picture.length; i++) {
+    let p = picture[i];
+    for (let r = 0; i < 10; r++) {
+      
+    }
   }
   
 }
