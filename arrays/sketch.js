b2a3f5c7a274a344f73ab42688ac768931507f44
Aiman918708 <aimanfatima384@gmail.com>
2026-03-17 00:11:46 -0600

    Update sketch.js

--- arrays/sketch.js
+++ arrays/sketch.js
@@ -12,17 +12,22 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background('black');
   
+  // Creates circles of random width, height and size, and adds them to the array
   for (let i = 0; i < 20; i++) {
     let circles = {
       x: random(windowWidth),
       y: random(windowHeight),
       radius: random(20,200)
     };
+    picture.push(circles);
   }
 }
 
 function draw() {
   background('black');
+  for (let i = 0; i < pictures.length; i++) {
+    if (i = circles)
+  }
   
 }
 
