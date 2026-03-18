a51a3175a6ebe0b3c7279f8a5238a58d15e5ddaa
Aiman918708 <aimanfatima384@gmail.com>
2026-03-18 03:03:24 -0600

    Update sketch.js
    finished assignment-  finished drawing and moving circles across the screen, switching colors, comments

--- arrays/sketch.js
+++ arrays/sketch.js
@@ -3,10 +3,10 @@
 // 16/03/26
 
 // Extra for Experts:
-// Using HSB instead of RGB values to control the brightness, saturation and transparency
-// of colors
+// Using HSB instead of RGB values to control color brightness, saturation and transparency
+// Look at 'switchColor()' function
 
-// Creating Arrays
+// Creating Array
 let picture = [];
 
 function setup() {
@@ -14,34 +14,79 @@ function setup() {
   colorMode(HSB, 360, 100, 100, 100);
   background(0); // 0 in HSB = to black
   
-  // Creates circles of random width, height and size, and adds them to the array
-  for (let i = 0; i < 20; i++) {
+  // Creates circles of random width, height and size
+  for (let i = 0; i < 100; i++) {
     let circles = {
+    // All objects are randomized
       x: random(windowWidth),
       y: random(windowHeight),
       radius: random(20,200),
-      c: color(random(300, 360), 100, 100, 20) 
+      theme: "pink", // Circles are shades of pink
+      c: color(random(294, 330), 100, 100, 70) ,
+      spdX: random(-10,10),
+      spdY: random(-10,10)
     };
+
+    // Adds circles to array
     picture.push(circles);
   }
 }
 
 function draw() {
-  // Draws the circles in the list onto the screen 
+  background(0);
+
+  // Goes through the array looking at the objects and drawing the circles accordingly
   for (let i = 0; i < picture.length; i++) {
     let p = picture[i];
 
-    noFill();
-    stroke(p.c);
-    strokeWeight(1);
-    
-    // Using 'r' for radius and 'd' for diameter
-    for (let r = 0; r < 10; r++) {
-      let d = (p.radius/10) * r;
-      ellipse(p.x, p.y, d)
-    }
+    moveCircles(p);
+    switchColor(p);  
+    drawCircles(p);
   }
-  noLoop();
-  
 }
 
+function drawCircles(p) {
+  noFill();
+  stroke(p.c);
+  strokeWeight(1);
+    
+  // Using 'r' for radius and 'd' for diameter
+  for (let r = 0; r < 20; r++) {
+    let d = (p.radius/20) * r;
+    ellipse(p.x, p.y, d)
+  }
+}
+
+function moveCircles(p) {
+  p.x += p.spdX;
+  p.y += p.spdY
+
+  // Lets circle change direction when it hits edge of screen
+  if (p.x > width || p.x < 0) {
+    p.spdX *= -1;
+  }
+
+  if (p.y > height || p.y < 0) {
+    p.spdY *= -1;
+  }
+}
+
+function switchColor(p) {
+  // When a circle hits the edge of the screen, it changes hues
+  if (p.x > width || p.x < 0 || p.y > height || p.y < 0) {
+    if (p.theme === "pink") {
+      p.c = color(random(176, 247), 100, 100, 70);
+      p.theme = "blue";
+    }
+
+    else if (p.theme === "blue") {
+      p.c = color(random(58, 150), 100, 100, 70);
+      p.theme = "yellow";
+    }
+
+    else if (p.theme === "yellow") {
+      p.c = color(random(294, 330), 100, 100, 70);
+      p.theme = "pink"
+    }
+  }
+}
