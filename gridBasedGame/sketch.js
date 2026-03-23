9af11f716a75eb33873edec92c272124b4b74af1
Aiman <aimanfatima384@gmail.com>
2026-03-23 11:02:47 -0600

    Started grid assignmnet, added comments for future reference

---
+++ gridBasedGame/sketch.js
@@ -0,0 +1,19 @@
+// 2D Grid Assignment
+// Aiman Fatima Adnan
+// 23/03/26
+
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+// Get picture and divide into cells
+// Create 2 Arrays, one for puzzle pieces and one empty
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+// Create functions for different levels
--- index.md
+++ index.md
@@ -4,6 +4,7 @@
 - [First Day Demo](01 - first demo)
 - [generative art] (01-art)
 - [arrays assignment] (arrays)
-- [2D Grid] (04-grid)
+- [2D Grid] (gameoflife)
 ## Assignments
-- assignments will be put here
+- [2D Grid] (grid - based game)
+
