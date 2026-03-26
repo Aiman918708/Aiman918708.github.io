e29a351457fc77e747e91d164e67edf33a86ccce
Aiman918708 <aimanfatima384@gmail.com>
2026-03-26 10:19:25 -0600

    Update sketch.js

--- gridBasedGame/sketch.js
+++ gridBasedGame/sketch.js
@@ -12,37 +12,41 @@ puzzleBoard = [];
 // Variables
 let cols;
 let rows;
-let level;
+let firstButton = {
+  w: 350,
+  h: 150
+};
+let secondButton = {
+  w: 350,
+  h: 150
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  firstButton.x = windowWidth/3 - (firstButton.w/1.5);
+  firstButton.y = windowHeight/3;
+
+  secondButton.x = (windowWidth*2)/3 - (secondButton.w/3);
+  secondButton.y =  windowHeight/3;
   // Two boards side by side
   
 }
 
 function draw() {
-  
+  showScreen();
+ // displayGrids();
 }
 
 // Functions
 
-function selectScreen() {
-  background('green');
-  if (level === 1) {
-    easyMode();
-  }
-  else if (level === 2) {
-    difficultMode();
-  }
+function showScreen() {
+  background(255, 255, 51);
+  strokeWeight(4);
+  stroke(255, 179, 222);
+  
+  fill(242, 15, 143);
+  rect(firstButton.x, firstButton.y, firstButton.w, firstButton.h, 20);
+  rect(secondButton.x, secondButton.y, secondButton.w, secondButton.h, 20);
 }
-
-function displayGrids() {
-
-}
-
-
-// Creating functions for different levels
-
-// function easyMode()
-// function difficultMode()
+  
