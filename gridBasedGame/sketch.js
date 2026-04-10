6022efceda07e008871d237942395cded1f11862
Aiman918708 <aimanfatima384@gmail.com>
2026-04-10 15:29:19 -0600

    made a folder for the images im gonna use, set grids

--- gridBasedGame/sketch.js
+++ gridBasedGame/sketch.js
@@ -6,35 +6,54 @@
 // - describe what you did to take this project "above and beyond"
 
 // Arrays
-puzzlePieces = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]];
-puzzleBoard = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]];
+puzzlePieces = [[0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0]];
+puzzleBoard = [[1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6]];
 
 // Variables
-let cols = 5;
-let rows = 5;
+let cols;
+let rows;
+let cellSize;
  
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellSize = width/20;
 }
 
 function draw() {
-  background(204,63,100);
-  displayGrids();
+  background('aqua');
+  showFirstGrid();
+  showSecondGrid();
 }
 
 // Functions
 
-function displayGrids() {
-  const CELL_SIZE = windowWidth/15;
-  let offset = windowWidth * 0.6;
+function showFirstGrid() {
+  let rows = puzzleBoard.length;
+  let cols = puzzleBoard[0].length;
 
-  rectMode(CENTER);
-  
+  // Nested for loop to display a 6x6 grid
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let x = i * cellSize + windowWidth/10;
+      let y = j * cellSize + windowHeight/27; 
 
-  for (let i = 0;i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      rect(CELL_SIZE * i + windowWidth/6, CELL_SIZE * j + windowHeight/5, CELL_SIZE, CELL_SIZE, 5);
-      rect(CELL_SIZE * i + offset, CELL_SIZE * j + windowHeight/5, CELL_SIZE, CELL_SIZE, 5);
+      rect(x, y, cellSize, cellSize);
+    }
+  }
+}
+
+function showSecondGrid() {
+  let rows = puzzlePieces[0].length;
+  let cols = puzzlePieces.length;
+  let gridWidth = rows * cellSize;
+
+  // Nested for loop displaying a 2x18 grid
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let x = i * cellSize + (windowWidth - gridWidth)/5;
+      let y = j * cellSize + (windowHeight)/1.325;
+
+      rect(x, y, cellSize, cellSize);
     }
   }
 }
