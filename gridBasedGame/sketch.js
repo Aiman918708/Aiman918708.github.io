67c7c04d16b27f4947cb69ed7f261729185f0d10
Aiman <aimanfatima384@gmail.com>
2026-04-13 11:02:29 -0600

    added picture to screen, positioned and resized it + repositioned the grid, changed background color

--- gridBasedGame/sketch.js
+++ gridBasedGame/sketch.js
@@ -9,18 +9,18 @@
 puzzlePieces = [[0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0]];
 puzzleBoard = [[1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6]];
 
-// Variables
+// Variables 
 let cols;
 let rows;
 let cellSize;
- 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellSize = width/20;
 }
 
 function draw() {
-  background('aqua');
+  background(255, 102, 102);
   showFirstGrid();
   showSecondGrid();
 }
@@ -34,9 +34,10 @@ function showFirstGrid() {
   // Nested for loop to display a 6x6 grid
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      let x = i * cellSize + windowWidth/10;
-      let y = j * cellSize + windowHeight/27; 
+      let x = i * cellSize + windowWidth * 0.125;
+      let y = j * cellSize + windowHeight/15; 
 
+      stroke(255, 102, 102);
       rect(x, y, cellSize, cellSize);
     }
   }
@@ -50,10 +51,23 @@ function showSecondGrid() {
   // Nested for loop displaying a 2x18 grid
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      let x = i * cellSize + (windowWidth - gridWidth)/5;
-      let y = j * cellSize + (windowHeight)/1.325;
+      let x = i * cellSize + (windowWidth - gridWidth)/2;
+      let y = j * cellSize + windowHeight/1.325;
 
       rect(x, y, cellSize, cellSize);
     }
   }
 }
+
+// Adding image next to grid
+const IMG = document.createElement('IMG');
+IMG.src = './images/picture.jpg';
+IMG.alt = 'Reference picture for the puzzle';
+document.body.appendChild(IMG);
+
+// Positioning and sizing image
+IMG.style.position = 'fixed';
+IMG.style.left = window.innerWidth/1.75 + 'px';
+IMG.style.top = window.innerHeight/15 + 'px';
+IMG.style.width = window.innerWidth/20 * 6 + 'px';
+IMG.style.height = window.innerWidth/20 * 6 + 'px';
