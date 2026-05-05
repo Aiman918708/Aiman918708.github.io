f2cb6c630f426e77f36914a5bf31bde7c659cf9c
Aiman <aimanfatima384@gmail.com>
2026-05-05 10:10:53 -0600

    collide demo

---
+++ collide/sketch.js
@@ -0,0 +1,28 @@
+// Collide Demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let hit;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  rect(200, 200, 100, 150);
+  circle(mouseX, mouseY, 100);
+
+  hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 100);
+
+  if (hit) {
+    fill('red');
+  }
+  else {
+    fill('white');
+  }
+}
